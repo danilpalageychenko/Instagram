0672ac55cd9d1d5e317a5ee91e7d4412a5684f08
danilpalageychenko <_dani4_@mail.ru>
2018-08-17 10:22:56 +0300

    Update instagram(original).js

--- instagram(original).js
+++ instagram(original).js
@@ -2,19 +2,17 @@ if (location.href.indexOf("instagram.com")==-1)
 {
 console.log("Скрипт необходимо запускать в чьём-то профиле Instagram.\nНапример, https://www.instagram.com/timatiofficial/")
 }
-else if (parseInt(document.getElementsByClassName("_bkw5z")[1].firstChild.parentElement.attributes[1].value.split(" ").join(""))<3000000)
-{
-console.log("На аккаунте " + document.getElementsByClassName("_i572c")[0].innerHTML + " недостаточно подписчиков для работы скрипта.")
-console.log("Выбери кого-то, у кого больше 3 миллионов подписчиков. \nНапример, https://www.instagram.com/timatiofficial/");
-}
 else
 {
 time=prompt("Введите количество секунд между действиями","30");
-a=document.getElementsByClassName("_ah57t");
+a=document.getElementsByClassName("vBF20 _1OSdk")[0].firstChild
 var i = 1;
-function nakr() {a[0].click();setTimeout(function(){a[0].click()}, 2000);console.log("---------------------------------------------" + i++);}
+function nakr() {
+a.click();
+if (b=document.getElementsByClassName("aOOlW -Cab_")[0])
+{
+setTimeout(function(){b.click()}, 2000);
+setTimeout(function(){a.click()}, 3000);
+}}
 setInterval(nakr,time*1000);
-console.log("Скрипт запущен.");
-console.log("Интервал между действиями равен " + time + " сек.");
-console.log("При закрытии данной вкладки работа скрипта будет завершена.");
 }
